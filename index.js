fa8232d098531c91cb0541a2b3824023ddd744b0
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-08-16 01:37:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Maya Angelou';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, welcomeMessage.slice[0]!`;
